eb4633582b19500fd501819de1f062a42ccf5193
Gavin <99065061@mydavinci.nl>
2021-10-14 13:36:35 +0200

    encounter

--- pokemon.py
+++ pokemon.py
@@ -5,5 +5,42 @@ starter = input("Which starter would you like to have by your side? You can choo
 print("Good choice! " + starter + " seems to like you! ")
 nameCheck = input("Would you like to name your Pokemon? y/n ")
 if nameCheck != "n":
-    nameStarter = input("Enter the desired name for your " + starter + " .")
-else: print(starter + " has joined your team!")
+    nameStarter = input("Enter the desired name for your " + starter + ". ")
+    print(nameStarter + " has joined your team!")
+else: 
+    print(starter + " has joined your team!")
+
+print("You walk through a patch of grass.")
+if starter == "chimchar":
+    encounter = "Budew"
+    print("You got attacked by Budew, a grass type pokemon! ") 
+elif starter == "turtwig": 
+    encounter = "Starly" 
+    print("You got attacked by Starly, a normal type pokemon!")
+elif starter == "prinplup":
+    encounter = "Bidoof" 
+    print("You got attacked by Bidoof, a normal type pokemon!")
+
+battle = input("Would you like to attack or catch the " + encounter + "? ")
+if battle == "attack" and nameCheck != "n":
+    print(nameStarter + " attacked!")
+    print(nameStarter + " has defeated " + encounter + "!")
+else:
+    print(starter + " attacked!")
+    print(starter + " has defeated " + encounter + "!")
+if battle == "catch":
+    print("You throw a pokeball at the opposing " + encounter + "!")
+    print("You caught the " + encounter + "!")
+encounterCheck = input("Would you like to name your " + encounter + "? ")
+if encounterCheck == "y":
+    encounterName = input("Enter the desired name for your " + encounter + ". ")
+    print(encounterName + " has joined the team!")
+else:
+    print(encounter + " has joined the team!")
+
+
+
+
+
+
+#ik maak een pokemon game voor school feelsgood
